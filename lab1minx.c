46bdc0250c17bdb9b0783910098afb6058246aec
Topx <153047996+6504062636012@users.noreply.github.com>
2024-11-28 17:57:14 +0700

    Change input from n1= n2= to n1 n2 using space " "

--- lab1minx.c
+++ lab1minx.c
@@ -3,14 +3,12 @@
 int main () {
 	
 	int n1,n2;
-	printf("n1=");
-	scanf("%d",&n1);
-	printf("n2=");
-	scanf("%d",&n2);
+	scanf("%d %d",&n1, &n2);
+
 	if 	(n1>n2){
-		printf("n1>n2  %d",n1-=n2);
+		printf("n1>n2 %d",n1-=n2);
 	}else {
-		printf("n2>n1  %d",n2-=n1);
+		printf("n2>n1 %d",n2-=n1);
 	}
 	
 }
